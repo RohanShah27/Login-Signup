5682342fc3a3fcfe6c5228c00f3d87f40d6cb4ea
Rohan <rohanshah2711@gmail.com>
2021-03-13 14:08:52 +0530

    documented encrypt middleware

--- server/src/middlewares/encrypt-decrypt.js
+++ server/src/middlewares/encrypt-decrypt.js
@@ -4,6 +4,7 @@ const CryptoJS = require("crypto-js");
 // ENCRYPTION KEY
 var key = "zvlfSIfNgBQR9EIASJu1";
 const encrypt = (data) => {
+  // Encrypt the data and convert to string
   var cipher = CryptoJS.AES.encrypt(data, key);
   cipher = cipher.toString();
   return cipher;
@@ -11,18 +12,24 @@ const encrypt = (data) => {
 
 const decrypt = (req, res, next) => {
   try {
+    //check if body has param as encrypted data
     if (!req.body.encryptedData) {
+      //if  not throw error
       throw {};
     }
+    // decrypt and convert to UTF8
     var decipher = CryptoJS.AES.decrypt(req.body.encryptedData, key);
     decipher = decipher.toString(CryptoJS.enc.Utf8);
+    //parse string to JSON and execute the next function
     req.body = JSON.parse(decipher);
     next();
   } catch (err) {
+    // send error if something goes wrong
     res.status(400).send({
       status: 406,
       message: "Key possibly incorrect",
     });
+    console.log("at:middlewares/encrypt-decrypt/decrypt", err);
   }
 };
 
